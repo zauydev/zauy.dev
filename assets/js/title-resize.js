c1c7732a2057e33fb38a0d19ff4acc8b9edb843a
Zauy <aviel.ziller@icloud.com>
2024-05-20 12:47:14 +0200

    Fixed: All animations. Shadow blur. Padding. Scrolling. To be fixed: small height jump upon page load.

--- assets/js/title-resize.js
+++ assets/js/title-resize.js
@@ -2,14 +2,12 @@ const container = document.getElementById("text-container");
 const section   = document.getElementById("title-section");
 const text      = document.getElementById("title-text");
 const blur      = document.getElementById("rainbow-blur");
-
-const triangle = document.getElementById('scroll-triangle')
+const triangle  = document.getElementById('scroll-triangle')
 
 function scrollable() {
     var scrollHeight = document.documentElement.scrollHeight;
     var scrollTop = window.scrollY || window.pageYOffset || document.body.scrollTop + (document.documentElement && document.documentElement.scrollTop || 0);
     var clientHeight = document.documentElement.clientHeight;
-
     return scrollTop + clientHeight < scrollHeight;
 }
 
@@ -20,26 +18,18 @@ function getScrollTop() {
 function checkScroll() {
     var scrollTop = getScrollTop();
 
-    if (scrollTop ==  0) {
+    if (scrollTop < 10) {
         console.log('scrollTop');
         container.classList.add('big-container');
         container.classList.remove('small-container'); 
-        blur.classList.add('big-blur');
-        blur.classList.remove('small-blur');
         text.classList.add('big-title');
         text.classList.remove('small-title');
-
     } else {
         container.classList.add('small-container');
         container.classList.remove('big-container');    
-        blur.classList.remove('big-blur');
-        blur.classList.add('small-blur');
         text.classList.remove('big-title');
         text.classList.add('small-title');
-
     }
-
-    console.log('scrolled');
 
     if (scrollable()) {
         triangle.style.opacity = 1;
@@ -50,6 +40,5 @@ function checkScroll() {
 }
 
 checkScroll();
-
 window.addEventListener('scroll', checkScroll);
 
